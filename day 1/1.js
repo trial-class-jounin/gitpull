023f0c3bde7f80c24e671a09dcef026e4dfad4da
nurmarf <45351487+nurmarf@users.noreply.github.com>
2019-09-08 11:48:10 +0700

    Add files via upload

---
+++ day 1/1.js
@@ -0,0 +1,12 @@
+var flag = 2;
+console.log('LOOPING PERTAMA')
+while(flag <21) { 
+  console.log(flag + ' I love coding' ); 
+  flag+=2; 
+}
+var flag1 = 20;
+console.log('LOOPING KEDUA')
+while(flag1 > 0) { 
+  console.log(flag1 + ' I will become fullstack developer' ); 
+  flag1-=2; 
+}
---
+++ day 1/2.js
@@ -0,0 +1,9 @@
+console.log("LOOPING PERTAMA")
+for(var angka = 1; angka < 20; angka++) {
+  console.log(angka + " - I love coding");
+}
+
+console.log("LOOPING KEDUA")
+for(var angka1 = 20; angka1 > 0; angka1--) {
+  console.log(angka1 + " - I will become fullstack developer");
+}
